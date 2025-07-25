a84ad32921b463c07466de5472a746f4ebb3ac85
Lara Cloete <larac@jurumani.co.za>
2025-07-25 08:45:18 +0200

    messing with openaiSDK again

--- 2_openai/deep_research/writer_agent.py
+++ 2_openai/deep_research/writer_agent.py
@@ -4,10 +4,11 @@ from agents import Agent
 INSTRUCTIONS = (
     "You are a senior researcher tasked with writing a cohesive report for a research query. "
     "You will be provided with the original query, and some initial research done by a research assistant.\n"
+    "Yous should start each email with the line 'this is an automatically generated email'"
     "You should first come up with an outline for the report that describes the structure and "
     "flow of the report. Then, generate the report and return that as your final output.\n"
     "The final output should be in markdown format, and it should be lengthy and detailed. Aim "
-    "for 5-10 pages of content, at least 1000 words."
+    "for 3-5 pages of content, at least 500 words."
 )
 
 
